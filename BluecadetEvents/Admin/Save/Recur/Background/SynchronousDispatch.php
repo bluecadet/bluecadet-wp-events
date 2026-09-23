773e44455f86aeed49d360dd5d763cc76da6501b
Shaun M Baer <shaun.baer@gmail.com>
2026-09-23 13:09:18 -0400

    Hotfix - allow for basic auth on WPE

--- BluecadetEvents/Admin/Save/Recur/Background/SynchronousDispatch.php
+++ BluecadetEvents/Admin/Save/Recur/Background/SynchronousDispatch.php
@@ -32,4 +32,27 @@ trait SynchronousDispatch {
 
     return parent::dispatch();
   }
+
+  /**
+   * Forward the current request's HTTP Basic Auth credentials to the loopback.
+   * Without this, a site behind basic auth (e.g. a password-protected WP Engine
+   * environment) answers the admin-ajax loopback with a 401 and the queue never
+   * runs.
+   *
+   * @return array
+   */
+  protected function get_post_args() {
+    $args = parent::get_post_args();
+
+    $auth = $_SERVER['HTTP_AUTHORIZATION'] ?? $_SERVER['REDIRECT_HTTP_AUTHORIZATION'] ?? '';
+    if ( ! $auth && isset( $_SERVER['PHP_AUTH_USER'] ) ) {
+      $auth = 'Basic ' . base64_encode( $_SERVER['PHP_AUTH_USER'] . ':' . ( $_SERVER['PHP_AUTH_PW'] ?? '' ) );
+    }
+
+    if ( $auth && empty( $args['headers']['Authorization'] ) ) {
+      $args['headers']['Authorization'] = $auth;
+    }
+
+    return $args;
+  }
 }
